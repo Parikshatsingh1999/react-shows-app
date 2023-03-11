eba60f5e48156c2cbcee922e4e74629789946774
Parikshat Singh <79864725+Parikshatsingh1999@users.noreply.github.com>
2023-03-11 16:41:48 +0530

    added reolved

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,33 +19,29 @@ export default function Header() {
 
     return (
         <>
-<div className='main-wrapper-header'>
-            <div className='logo-box head-item'> <img src={img} alt="logo" /> </div>
-            <div className='title-box head-item'>
-                <h2 onClick={() => sendHome()}>  {name} </h2>
+            <div className='main-wrapper-header'>
+                <div className='logo-box head-item'> <img src={img} alt="logo" /> </div>
+                <div className='title-box head-item'>
+                    <h2 onClick={() => sendHome()}>  {name} </h2>
+                </div>
             </div>
-        </div> 
-      <div class="main_page">
-        <div class="images">
-        <img src={bgimg} alt="bg-image" class="desktop" />
-        <img src={bgimgM} alt="bg-image" class="mobiled" />
-        </div>
-            <div class="content_part">
-        <div>
-            <h1 class="main_headingss">Search for Unlimited movies, TV shows and more.</h1>
-        </div>
-        <div className='search-box head-item'>
-                <input placeholder='Search for your favourite shows' value={query} onInput={(e) => setQuery(e.target.value)} />
-                <button onClick={() => handleSearch()}> Search </button>
-            </div>
-            <div className='search-box head-item'>
-                <input ref={queryRef} />
-                <button onClick={() => handleSearch()}> Search </button>
-            </div>
-            <div className='title-box head-item'>
-                <h2 onClick={() => sendHome()}>  {name} </h2>
-            </div>
-            </div>
+            <div class="main_page">
+                <div class="images">
+                    <img src={bgimg} alt="bg-image" class="desktop" />
+                    <img src={bgimgM} alt="bg-image" class="mobiled" />
+                </div>
+                <div class="content_part">
+                    <div>
+                        <h1 class="main_headingss">Search for Unlimited movies, TV shows and more.</h1>
+                    </div>
+                    <div className='search-box head-item'>
+                        <input ref={queryRef} />
+                        <button onClick={() => handleSearch()}> Search </button>
+                    </div>
+                    <div className='title-box head-item'>
+                        <h2 onClick={() => sendHome()}>  {name} </h2>
+                    </div>
+                </div>
             </div>
         </>
     )
